8c86feb4214abb8d395ef32f271a3439736eb5a0
Kohei Ando <andokohei@gmail.com>
2018-04-24 10:20:09 +0000

    test added

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ const DB_CONF = require('config').db;
 var p = console.log();
 
 var master = new DBC(DB_CONF.master, 'master');
-var slave1 = new DBC(DB_CONF.slave1, 'slave1');
-var slave2 = new DBC(DB_CONF.slave2, 'slave2'); 
+// var slave1 = new DBC(DB_CONF.slave1, 'slave1');
+// var slave2 = new DBC(DB_CONF.slave2, 'slave2'); 
 
-function randomClient(){
-    var i = Date.now()/3;
-    return i == 0 ? master : 1 ? slave1 : slave2;
-}
+// function randomClient(){
+//     var i = Date.now()/3;
+//     return i == 0 ? master : 1 ? slave1 : slave2;
+// }
 
 module.exports = { 
-    master : master, 
-    slave1: slave1, 
-    slave2: slave2,
-    write: master,
-    read: randomClient()
+    master : master
+    // slave1: slave1, 
+    // slave2: slave2,
+    // write: master,
+    // read: randomClient()
 };
--- test/test.js
+++ test/test.js
@@ -11,3 +11,18 @@ var assert = chai.assert,
   expect = chai.expect,
   should = chai.should(),
   p = console.log;
+
+var db_client = require('../src/index');
+
+describe('DBClient', function(){
+  describe('query with pool', function(){
+    it('should return an array of objects', function(){
+      var q = 'SELECT * FROM user'
+      return db_client.master.query(q)
+        .then(res => {
+          expect(res).to.be.an('array');
+          return res.map(e => expect(e).to.be.instanceOf(Object))
+        })
+    })
+  })
+})
