8ddae819da29cf8e8df23e770cd0559bfe1c2e31
Kohei Ando <andokohei@gmail.com>
2018-04-25 01:21:15 +0000

    added some more tests

--- test/test.js
+++ test/test.js
@@ -12,12 +12,13 @@ var assert = chai.assert,
   should = chai.should(),
   p = console.log;
 
+var mysql = require('mysql2');
 var db_client = require('../src/index');
 
 describe('DBClient', function(){
   describe('query with pool', function(){
     it('should return an array of objects', function(){
-      var q = 'SELECT * FROM user'
+      var q = 'SELECT * FROM user';
       return db_client.master.query(q)
         .then(res => {
           expect(res).to.be.an('array');
@@ -25,4 +26,24 @@ describe('DBClient', function(){
         })
     })
   })
-})
+  describe('execute with pool', function(){
+    it('should return an array of objects', function(){
+      var q = 'SELECT * FROM user WHERE id = ?';
+      return db_client.master.execute(q, [1])
+        .then(res => {
+          expect(res).to.be.an('array');
+          return res.map(e => expect(e).to.be.instanceOf(Object))
+        })
+    })
+  })
+  describe('getSingleConnection', function(){
+    it('should return a MySqlConnection object', function(){
+      return db_client.getSingleConnection()
+        .then(c => {
+          expect(c).to.be.instanceof(mysql.MySqlConnection);
+          c.release;
+          return
+        })
+    })
+  })
+});
